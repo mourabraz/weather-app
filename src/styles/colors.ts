275a476173e2fb7fb6425be668018653e9634c21
mourabraz <cmourabraz@gmail.com>
2021-05-20 10:11:04 +0100

    add style colors

---
+++ src/styles/colors.ts
@@ -0,0 +1,21 @@
+// eslint-disable-next-line no-shadow
+export enum Colors {
+  primary = '#2196F3',
+  primaryLight = '#BBDEFB',
+  primaryDark = '#1976D2',
+
+  textIcons = '#FFFFFF',
+  primaryText = '#212121',
+  secondaryText = '#757575',
+
+  accent = '#03A9F4',
+  divider = '#BDBDBD',
+
+  default = '#9E9E9E',
+  defaultLight = '#F5F5F5',
+  defaultDark = '#616161',
+
+  error = '#F44336',
+  errorLight = '#FFCDD2',
+  errorDark = '#D32F2F',
+}
