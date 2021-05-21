d6e59d495ec1f9b37d23e6f8956ff5b5d959e973
mourabraz <cmourabraz@gmail.com>
2021-05-21 10:31:40 +0100

    add home components

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,42 @@
 import React from 'react';
 
-import { Container, Content } from './styles';
+import { Container, TemperatureInfo, InfoBox } from './styles';
 
 export const Home: React.FC = () => {
   return (
     <Container>
-      <Content>
-        <span>Cidade</span>
-      </Content>
+      <h1>Cortes, Leiria</h1>
+      <TemperatureInfo>
+        <h2>
+          <span>icon</span> 18º <span>C</span>
+        </h2>
+        <h3>Geralmente nublado</h3>
+        <p>Última atualzação às 09:53</p>
+      </TemperatureInfo>
+      <InfoBox>
+        <div>
+          <p>
+            Sensação Térmica <span>17º</span>
+          </p>
+          <p>
+            Vento <span>sudoeste</span> <span>9 km/h</span>
+          </p>
+          <p>
+            Visibilidade <span>10 km</span>
+          </p>
+        </div>
+        <div>
+          <p>
+            Barómetro <span>1019,00 mb</span>
+          </p>
+          <p>
+            Humidade <span>83%</span>
+          </p>
+          <p>
+            Ponto de Condensação <span>14º</span>
+          </p>
+        </div>
+      </InfoBox>
     </Container>
   );
 };
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,5 +1,70 @@
 import styled from 'styled-components';
+import { Colors } from '../../styles/colors';
 
-export const Container = styled.section``;
+export const Container = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: ${Colors.primaryText};
 
-export const Content = styled.div``;
+  h1 {
+    font-weight: normal;
+  }
+`;
+
+export const TemperatureInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 32px 0;
+
+  h2 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 5rem;
+    margin-bottom: 8px;
+
+    span {
+      font-size: 1.6rem;
+      margin: 0 8px;
+    }
+  }
+
+  h3 {
+    font-weight: normal;
+    font-size: 1.8rem;
+    margin-bottom: 8px;
+  }
+
+  p {
+    font-size: 1.2rem;
+    font-style: italic;
+  }
+`;
+
+export const InfoBox = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  > div {
+    display: flex;
+    width: 100%;
+    justify-content: center;
+
+    & + div {
+      margin-top: 8px;
+    }
+
+    p {
+      & + p {
+        margin-left: 32px;
+      }
+
+      span {
+        margin-left: 8px;
+      }
+    }
+  }
+`;
