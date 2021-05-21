086f237a408921261e105b6c28d60c31d01158f5
mourabraz <cmourabraz@gmail.com>
2021-05-21 09:12:15 +0100

    add Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
+import { Default } from './pages/_layouts/Default';
+import { Home } from './pages/Home';
+
 import GlobalStyle from './styles/global';
 
 export const App: React.FC = () => {
   return (
     <>
-      <h1>App</h1>
+      <Default>
+        <Home />
+      </Default>
       <GlobalStyle />
     </>
   );
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container, Content } from './styles';
+
+export const Home: React.FC = () => {
+  return (
+    <Container>
+      <Content>
+        <span>Cidade</span>
+      </Content>
+    </Container>
+  );
+};
