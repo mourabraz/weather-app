0f7ecd5816c973a45c1933d41f6958eb7e17372d
mourabraz <cmourabraz@gmail.com>
2021-05-23 20:53:38 +0100

    add devtool for redux

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,14 @@
 import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(() => []);
+import { Position } from '../interfaces';
+
+import rootReducer from './modules/rootReducer';
+
+export interface State {
+  position: Position;
+}
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 export default store;
