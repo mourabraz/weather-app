c8d948eb791c54cddd58460e493076dccdb8be59
mourabraz <cmourabraz@gmail.com>
2021-05-24 18:50:17 +0100

    refactor header layout

--- src/pages/_layouts/Default/components/Header/index.tsx
+++ src/pages/_layouts/Default/components/Header/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useCallback, useMemo } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { FiRefreshCw, FiStar, FiSearch } from 'react-icons/fi';
 
 import { Button } from '../../../../../components/Button';
@@ -6,14 +7,40 @@ import { Input } from '../../../../../components/Input';
 import { Colors } from '../../../../../styles/colors';
 
 import { Container } from './styles';
+import {
+  refresh,
+  toggleFavoritesRequest,
+} from '../../../../../store/modules/manager/actions';
+import { State } from '../../../../../store';
 
 export const Header: React.FC = () => {
+  const dispatch = useDispatch();
+  const favorites = useSelector((state: State) => state.manager.favorites);
+  const current = useSelector((state: State) => state.currentForecast);
+
+  const isFavorite = useMemo(() => {
+    return current ? !!favorites.find(i => i.id === current.city.id) : false;
+  }, [current, favorites]);
+
+  const handleRefresh = useCallback(() => {
+    dispatch(refresh());
+  }, [dispatch]);
+
+  const handleFavoriteToggle = useCallback(() => {
+    dispatch(toggleFavoritesRequest());
+  }, [dispatch]);
+
   return (
     <Container>
-      <Button icon={FiRefreshCw} title="Refresh">
+      <Button icon={FiRefreshCw} title="Refresh" onClick={handleRefresh}>
         refresh
       </Button>
-      <Button icon={FiStar} title="Add/Remove from favourites">
+      <Button
+        icon={FiStar}
+        fillIcon={isFavorite ? Colors.textIcons : 'none'}
+        title={`${isFavorite ? 'Remove' : 'Add'} from favourites`}
+        onClick={handleFavoriteToggle}
+      >
         favourite
       </Button>
       <Input
